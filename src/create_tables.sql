093f687fe6d12700674c295a6674027715f27143
Jacob Reckhard <jacobreckhard@gmail.com>
2019-10-18 13:27:33 -0600

    create tables sql

---
+++ src/create_tables.sql
@@ -0,0 +1,106 @@
+drop table if exists demeritNotices;
+drop table if exists tickets;
+drop table if exists registrations;
+drop table if exists vehicles;
+drop table if exists marriages;
+drop table if exists births;
+drop table if exists persons;
+drop table if exists payments;
+drop table if exists users;
+
+PRAGMA foreign_keys = ON;
+
+create table persons (
+  fname		char(12),
+  lname		char(12),
+  bdate		date,
+  bplace	char(20),
+  address	char(30),
+  phone		char(12),
+  primary key (fname, lname)
+);
+create table births (
+  regno		int,
+  fname		char(12),
+  lname		char(12),
+  regdate	date,
+  regplace	char(20),
+  gender	char(1),
+  f_fname	char(12),
+  f_lname	char(12),
+  m_fname	char(12),
+  m_lname	char(12),
+  primary key (regno),
+  foreign key (fname,lname) references persons,
+  foreign key (f_fname,f_lname) references persons,
+  foreign key (m_fname,m_lname) references persons
+);
+create table marriages (
+  regno		int,
+  regdate	date,
+  regplace	char(20),
+  p1_fname	char(12),
+  p1_lname	char(12),
+  p2_fname	char(12),
+  p2_lname	char(12),
+  primary key (regno),
+  foreign key (p1_fname,p1_lname) references persons,
+  foreign key (p2_fname,p2_lname) references persons
+);
+create table vehicles (
+  vin		char(5),
+  make		char(10),
+  model		char(10),
+  year		int,
+  color		char(10),
+  primary key (vin)
+);
+create table registrations (
+  regno		int,
+  regdate	date,
+  expiry	date,
+  plate		char(7),
+  vin		char(5),
+  fname		char(12),
+  lname		char(12),
+  primary key (regno),
+  foreign key (vin) references vehicles,
+  foreign key (fname,lname) references persons
+);
+create table tickets (
+  tno		int,
+  regno		int,
+  fine		int,
+  violation	text,
+  vdate		date,
+  primary key (tno),
+  foreign key (regno) references registrations
+);
+create table demeritNotices (
+  ddate		date,
+  fname		char(12),
+  lname		char(12),
+  points	int,
+  desc		text,
+  primary key (ddate,fname,lname),
+  foreign key (fname,lname) references persons
+);
+create table payments (
+  tno		int,
+  pdate		date,
+  amount	int,
+  primary key (tno, pdate),
+  foreign key (tno) references tickets
+);
+create table users (
+  uid		char(8),
+  pwd		char(8),
+  utype		char(1),	-- 'a' for agents, 'o' for officers
+  fname		char(12),
+  lname		char(12),
+  city		char(15),
+  primary key(uid),
+  foreign key (fname,lname) references persons
+);
+
+
