4f2478def704626857505a768fc510631ffde164
Jacob Reckhard <jacobreckhard@gmail.com>
2019-10-20 20:38:57 -0600

    made cross platform?

--- src/utils/password.rkt
+++ src/utils/password.rkt
@@ -1,14 +1,22 @@
 #lang racket
 
+(require ffi/unsafe)
 (provide read-password)
 
-(require ffi/unsafe)
-(define libm (ffi-lib "/usr/lib/libc-2.30"))
+
+(define has-libc #t)
+(define read-password (lambda (prompt)
+                        (begin
+                          (displayln prompt)
+                          (read-line))))
+(define libm (ffi-lib "/usr/lib/libc-2.30"
+                      #:fail (lambda () (set! has-libc #f))))
 
 
 ; Usage
 ; (displayln (read-password "Prompt:"))
-(define read-password
-  (get-ffi-obj 'getpass libm (_fun _string/utf-8 -> _string/utf-8)
-         (lambda () (read-line))))
+(when has-libc
+  (set! read-password
+    (get-ffi-obj 'getpass libm (_fun _string/utf-8 -> _string/utf-8)
+           (lambda () (read-line)))))
 
