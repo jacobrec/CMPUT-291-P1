cd5f0bad9c2a85d8079cadf530a87fd61d5661e2
Jacob Reckhard <jacobreckhard@gmail.com>
2019-10-30 13:23:32 -0600

    get-pass fixed for lab machines

--- src/utils/password.rkt
+++ src/utils/password.rkt
@@ -4,14 +4,11 @@
 (require "io.rkt")
 (provide read-password)
 
-(define has-libc #t)
 (define read-password prompt)
-(define libm (ffi-lib "/usr/lib/libc-2.30"
-                      #:fail (lambda () (set! has-libc #f))))
+(define libm (ffi-lib #f))
 
 ; Usage
 ; (displayln (read-password "Prompt:"))
-(when has-libc
-  (set! read-password
-    (get-ffi-obj 'getpass libm (_fun _string/utf-8 -> _string/utf-8)
-           (lambda () (read-line)))))
+(set! read-password
+  (get-ffi-obj 'getpass libm (_fun _string/utf-8 -> _string/utf-8)
+         (lambda () (prompt))))
