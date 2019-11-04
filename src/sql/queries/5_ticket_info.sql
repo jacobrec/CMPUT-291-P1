5285b25b52471e01910bcb317d36dd1f67d18749
Radomir <rasdasdasdgoa@gmail.com>
2019-11-04 15:46:43 -0700

    Fixed future babies and multiple payments on the same day. Added sparse comments to my functionalities.

--- src/sql/queries/5_ticket_info.sql
+++ src/sql/queries/5_ticket_info.sql
@@ -1,3 +1,3 @@
-SELECT regno, fine, violation, vdate, IFNULL(SUM(amount), 0)
+SELECT regno, fine, violation, vdate, IFNULL(SUM(amount), 0), IFNULL(MAX(pdate), '0000-01-01')
 FROM tickets AS t LEFT OUTER JOIN payments AS p USING (tno)
 GROUP BY tno HAVING tno=:tno;
